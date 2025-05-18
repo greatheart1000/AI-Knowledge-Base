4a8598b33ecf1885bda11e9a1c250df4e5168562
greatheart1000 <64632461+greatheart1000@users.noreply.github.com>
2025-05-18 15:12:30 +0800

    Update car_train.py

--- car_train.py
+++ car_train.py
@@ -10,11 +10,10 @@ from transformers import (
     Trainer,
     DataCollatorForSeq2Seq,
     Qwen2VLForConditionalGeneration,
-    AutoProcessor,
+    AutoProcessor,DefaultDataCollator
 )
 import swanlab
 import json
-
 
 def process_func(example):
     """
@@ -43,7 +42,20 @@ def process_func(example):
     text = processor.apply_chat_template(
         messages, tokenize=False, add_generation_prompt=True
     )  # 获取文本
-    image_inputs, video_inputs = process_vision_info(messages)  # 获取数据数据（预处理过）
+
+    image_inputs, video_inputs = [], []
+    try:
+        image_inputs, video_inputs = process_vision_info(messages)  # 获取预处理后的图像和视频数据
+    except OSError as e:
+        if "image file is truncated" in str(e):
+            print(f"Warning: Skipping truncated image: {file_path}")
+            image_inputs = [torch.zeros((3, 224, 224))]  # 使用一个零张量作为占位符
+        else:
+            raise e
+    # 处理 video_inputs 为空的情况，如果为空则设置为 None
+    if not video_inputs:
+        video_inputs = None
+
     inputs = processor(
         text=[text],
         images=image_inputs,
@@ -51,21 +63,20 @@ def process_func(example):
         padding=True,
         return_tensors="pt",
     )
-    inputs = {key: value.tolist() for key, value in inputs.items()} #tensor -> list,为了方便拼接
+    inputs = {key: value.tolist() for key, value in inputs.items()} # tensor -> list, 为了方便拼接
     instruction = inputs
 
     response = tokenizer(f"{output_content}", add_special_tokens=False)
 
-
     input_ids = (
-            instruction["input_ids"][0] + response["input_ids"] + [tokenizer.pad_token_id]
+        instruction["input_ids"][0] + response["input_ids"] + [tokenizer.pad_token_id]
     )
 
     attention_mask = instruction["attention_mask"][0] + response["attention_mask"] + [1]
     labels = (
-            [-100] * len(instruction["input_ids"][0])
-            + response["input_ids"]
-            + [tokenizer.pad_token_id]
+        [-100] * len(instruction["input_ids"][0])
+        + response["input_ids"]
+        + [tokenizer.pad_token_id]
     )
     if len(input_ids) > MAX_LENGTH:  # 做一个截断
         input_ids = input_ids[:MAX_LENGTH]
@@ -76,7 +87,7 @@ def process_func(example):
     attention_mask = torch.tensor(attention_mask)
     labels = torch.tensor(labels)
     inputs['pixel_values'] = torch.tensor(inputs['pixel_values'])
-    inputs['image_grid_thw'] = torch.tensor(inputs['image_grid_thw']).squeeze(0)  #由（1,h,w)变换为（h,w）
+    inputs['image_grid_thw'] = torch.tensor(inputs['image_grid_thw']).squeeze(0)  # 由（1,h,w)变换为（h,w）
     return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels,
             "pixel_values": inputs['pixel_values'], "image_grid_thw": inputs['image_grid_thw']}
 
@@ -116,24 +127,13 @@ tokenizer = AutoTokenizer.from_pretrained("./Qwen", use_fast=False, trust_remote
 processor = AutoProcessor.from_pretrained("./Qwen")
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained("./Qwen", device_map="auto", trust_remote_code=True,)
-#model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
+model.enable_input_require_grads()  # 开启梯度检查点时，要执行该方法
 
 # 处理数据集：读取json文件
 # 拆分成训练集和测试集，保存为data_vl_train.json和data_vl_test.json
-train_json_path = "Dcar_1000.json"
-with open(train_json_path, 'r') as f:
-    data = json.load(f)
-    train_data = data[:-4]
-    test_data = data[-4:]
 
-with open("Dcar_1000_train.json", "w") as f:
-    json.dump(train_data, f)
-
-with open("Dcar_1000_test.json", "w") as f:
-    json.dump(test_data, f)
-
-#train_ds = Dataset.from_json("Dcar_1000_train.json")
-#train_dataset = train_ds.map(process_func)
+train_ds = Dataset.from_json("data_vl.json")
+train_dataset = train_ds.map(process_func)
 
 # 配置LoRA
 config = LoraConfig(
@@ -156,7 +156,7 @@ args = TrainingArguments(
     gradient_accumulation_steps=4,
     logging_steps=10,
     logging_first_step=5,
-    num_train_epochs=4,
+    num_train_epochs=1,
     save_steps=100,
     learning_rate=1e-4,
     save_on_each_node=True,
@@ -173,8 +173,8 @@ swanlab_callback = SwanLabCallback(
         "model": "https://modelscope.cn/models/Qwen/Qwen2.5-VL-3B-Instruct",
         "dataset": "https://modelscope.cn/datasets/modelscope/coco_2014_caption/quickstart",
         "github": "https://github.com/datawhalechina/self-llm",
-        "prompt": "Dcar Yes: ",
-        "train_data_number": len(train_data),
+        "prompt": f"请你分析一下此图片为汽车的内饰图片还是外饰图片,是什么点位类型、是什么点位名称",
+        "train_data_number": 4267,
         "lora_rank": 64,
         "lora_alpha": 16,
         "lora_dropout": 0.1,
@@ -182,16 +182,16 @@ swanlab_callback = SwanLabCallback(
 )
 
 # 配置Trainer
-#trainer = Trainer(
-#    model=peft_model,
-#    args=args,
-#    train_dataset=train_dataset,
-#    data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True),
-#    callbacks=[swanlab_callback],
-#)
+trainer = Trainer(
+    model=peft_model,
+    args=args,
+    train_dataset=train_dataset,
+    data_collator=DefaultDataCollator(return_tensors="pt", padding=True),
+    callbacks=[swanlab_callback],
+)
 
 # 开启模型训练
-#trainer.train()
+trainer.train()
 
 # ====================测试模式===================
 # 配置测试参数
