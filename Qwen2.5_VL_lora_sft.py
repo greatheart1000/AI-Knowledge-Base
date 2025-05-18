320c81fd7f14090ac9e791436a3559a526c318f5
greatheart1000 <64632461+greatheart1000@users.noreply.github.com>
2025-05-18 19:16:57 +0800

    Create Qwen2.5_VL_lora_sft.py

---
+++ Qwen2.5_VL_lora_sft.py
@@ -0,0 +1,4 @@
+训练时 OSError: image file is truncated (** bytes not processed) 问题解决 链接：
+以下两个链接： 
+https://blog.csdn.net/chhj_292/article/details/137281590 
+https://blog.csdn.net/scool_winter/article/details/89426509?spm=1001.2101.3001.6650.1&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7EPaidSort-1-89426509-blog-137281590.235%5Ev43%5Epc_blog_bottom_relevance_base2&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7EPaidSort-1-89426509-blog-137281590.235%5Ev43%5Epc_blog_bottom_relevance_base2&utm_relevant_index=1
